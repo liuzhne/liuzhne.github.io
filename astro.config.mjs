12c1ce6cd447659889f1e93b0dddb2f9a6ea50da
derrick <derrick@derrickdeMacBook-Pro.local>
2026-04-06 22:18:07 +0800

    remove /blog/

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://liuzhne.github.io',
-  base: '/blog',
   trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()]
